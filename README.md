b820d8af7875cc45743e3326e510d6bd4459f3d2
Baoshuo <i@baoshuo.ren>
2022-03-29 19:08:18 +0800

    feat: init

---
+++ README.md
@@ -0,0 +1,30 @@
+# Codeforces Rating Badge
+
+## Demo
+
+![](https://cfrating.baoshuo.dev/rating?username=baoshuo)
+
+## Deploy
+
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Frenbaoshuo%2Fcodeforces-rating)
+
+## Usage
+
+Just insert the link into your website as an image, like this: `<img src="https://cfrating.baoshuo.dev/rating?username=baoshuo">`
+
+Do not forget to replace the default configuration with your own!
+
+---
+
+Parameter list:
+
+| Name     | Value  | Required | Description              |
+| -------- | ------ | -------- | ------------------------ |
+| username | string | Yes      | Your Codeforces username |
+
+## Author
+
+**Codeforces Rating Badge** © [Baoshuo](https://github.com/renbaoshuo), Released under the [MIT](./LICENSE) License.<br>
+Authored and maintained by Baoshuo with help from contributors ([list](https://github.com/renbaoshuo/codeforces-rating/graphs/contributors)).
+
+> [Personal Website](https://baoshuo.ren) · [Blog](https://blog.baoshuo.ren) · GitHub [@renbaoshuo](https://github.com/renbaoshuo) · Twitter [@renbaoshuo](https://twitter.com/renbaoshuo)
---
+++ api/rating.ts
@@ -0,0 +1,46 @@
+import fetch from 'node-fetch';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+function getRatingColor(rating: number) {
+    if (rating >= 2400) return 'ff0000';
+    if (rating >= 2100) return 'ff8c00';
+    if (rating >= 1900) return 'aa00aa';
+    if (rating >= 1600) return '0000ff';
+    if (rating >= 1400) return '03a89e';
+    if (rating >= 1200) return '008000';
+    return '808080';
+}
+
+export default (request: VercelRequest, response: VercelResponse) => {
+    const { username } = request.query;
+
+    fetch(`https://codeforces.com/api/user.info?handles=${username}`)
+        .then((res) => {
+            if (!res.ok) throw '';
+            return res.json();
+        })
+        .then((res) => {
+            if (!res.result.length) throw '';
+
+            const { rank, rating } = res.result[0];
+            const color = getRatingColor(rating);
+            const escapedUsername = (username as string).replace(/-/g, '--').replace(/_/g, '__');
+
+            fetch(
+                `https://img.shields.io/badge/${escapedUsername}-${rank}  ${rating}-${color}.svg?longCache=true&style=for-the-badge&link=https://codeforces.com/profile/${username}&logo=codeforces`
+            )
+                .then((res) => {
+                    if (!res.ok) throw '';
+                    return res.text();
+                })
+                .then((res) => {
+                    response.status(200).setHeader('Content-Type', 'image/svg+xml;charset=utf-8').send(res);
+                })
+                .catch((err) => {
+                    response.status(500).send('Error!\n' + err);
+                });
+        })
+        .catch((err) => {
+            response.status(500).send('Error!\n' + err);
+        });
+};
